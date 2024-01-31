2687b1160aed174ac11c9917caad45e037652e81
Bekarys <abievb91@gmail.com>
2024-01-31 19:50:52 +0600

    code

---
+++ lab1/PYTHON COMMENTS EXERCISE/1.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+if a > b:
+    print("true")  #This is comment
+else:
+    print ("false")  #second comment
---
+++ lab1/PYTHON COMMENTS EXERCISE/2.py
@@ -0,0 +1,4 @@
+a = 5
+b = "Bekarys" #this is my name
+print(a)
+print(b)
